300103169d39d3892dfe10fdc6c004db44dc05b4
Brendanchoi626 <18671@burnside.school.nz>
2021-05-28 14:21:20 +1200

    signup page done (no css)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+# imports
+from flask import Flask, render_template, request, session, redirect, url_for, Blueprint, flash
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+
 #from main import db
 
 app = Flask(__name__)
@@ -12,23 +14,49 @@ import models
 #routes#
 @app.route('/')
 def home():
-    return render_template('home.html', title='home')
+    post = models.Post.query.all()
+    return render_template('home.html', post=post, title='home')
 
-@app.route('/post/')
+
+@app.route('/post')
 def post():
     return render_template('post.html', title='post')
 
+
 @app.route('/noti/<int:id>')
 def noti(id):
+
     return render_template('noti.html', title='noti')
+
 
 @app.route('/profile/<int:id>')
 def user(id):
     return render_template('profile.html', title='profile')
 
-@app.route('/signup/')
-def signup():
+
+@app.route('/signup', methods = ['GET', 'POST'])
+def signup():#Signup route 
+    if request.method == 'POST':#Checks if the users can create a new account with input information
+        if models.User.query.filter_by(username = request.form.get('username')).first() != None:
+            return render_template('signup.html', error = 'username already in use')
+        if models.User.query.filter_by(email = request.form.get('email')).first() != None:
+            return render_template('signup.html', error = 'email already in use')
+        if len(request.form.get('password')) < 8:
+            return render_template('signup.html', error='password must be atleast 8 letters')
+        if request.form.get('password') != request.form.get('re-password'):
+            return render_template('signup.html', error = 'your passwords do not match. Please try again')
+        else:# if possible, create an account. 
+            user_info = models.User(
+
+                username = request.form.get('username'),
+                email = request.form.get('email'),
+                password = request.form.get('password')
+
+            )
+            db.session.add(user_info)
+            db.session.commit()
     return render_template('signup.html',title='signup')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models.py
+++ models.py
@@ -22,34 +22,51 @@ PostUser = db.Table('PostUser', db.Model.metadata,
 
 
 class User(db.Model):
+    __tablename__ = 'User'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.Text)
     email = db.Column(db.Text)
     password = db.Column(db.Text)
 
+    posts_user = db.relationship('Post', secondary=PostUser, back_populates='users_post')
+    notifications_user = db.relationship('Notification', secondary=UserNotification, back_populates='users_notification')
 
 
 class Reply(db.Model):
+    __tablename__ = 'Reply'
     id = db.Column(db.Integer, primary_key=True)
     reply = db.Column(db.Text)
     date = db.Column(db.Integer)
 
+    posts_reply = db.relationship('Post', secondary=PostReply, back_populates='replies_post')
+
 
 class Post(db.Model):
+    __tablename__ = 'Post'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.Text)
     discussion = db.Column(db.Text)
     date = db.Column(db.Integer)
     likes = db.Column(db.Integer)
 
+    users_post = db.relationship('User', secondary=PostUser, back_populates='posts_user')
+    replies_post = db.relationship('Reply', secondary=PostReply, back_populates='posts_reply')
+    categories_post = db.relationship('Categories', secondary=PostCategory, back_populates='posts_category')
+
 
 class Notification(db.Model):
+    __tablename__ = 'Notification'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.Text)
     content = db.Column(db.Text)
 
+    users_notification = db.relationship('User', secondary=UserNotification, back_populates='notifications_user')
+
 
 class Categories(db.Model):
+    __tablename__ = 'Categories'
     id = db.Column(db.Integer, primary_key=True)
     topic = db.Column(db.Text)
+
+    posts_category = db.relationship('Post', secondary=PostCategory, back_populates='categories_post')
 
